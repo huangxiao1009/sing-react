361e3b318e86fb51d2c461e9ac91c6f04d1b2cd7
huangxiao <huangxiao@mail.51vv.com>
2019-01-21 18:10:54 +0800

    0121

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,23 +4,26 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
-module.exports = merge(common,{
-    mode: 'development',
-    devtool:'inline-source-map',
-    devServer:{
-        contentBase:path.join(__dirname,'dist'),
-        compress:true,
-        port: 9000,
-        index:'html/index.html',
-        openPage:'html/index.html',
-        // hot:true,
-        // noInfo:true
-    },
-    plugins:[
-        //默认添加NODE_ENV为development
-        new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("development") }),
-        //模块热更新
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ]
-});
+module.exports = env =>{
+    console.log('env:',env);
+    merge(common,{
+        mode: 'development',
+        devtool:'inline-source-map',
+        devServer:{
+            contentBase:path.join(__dirname,'dist'),
+            compress:true,
+            port: 9000,
+            index:'html/index.html',
+            openPage:'html/index.html',
+            // hot:true,
+            // noInfo:true
+        },
+        plugins:[
+            //默认添加NODE_ENV为development
+            new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("development") }),
+            //模块热更新
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+};
