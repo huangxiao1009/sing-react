510d8ec03e722b2150f31a8b1e307e559125be3f
huangxiao <huangxiao@mail.51vv.com>
2019-01-24 09:47:51 +0800

    0124

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 /**
  * Created by huangxiao on 2019/1/22.
  */
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 
 import {render} from 'react-dom';
 
 render(
-    <div>hello</div>,
+    <div>
+        hello,{22 + 12}
+        {/*这是注释*/}
+    </div>,
     document.getElementById('app')
 );
